0b6ae210e6a07c9ce7fedcdd7ff94133274a7a04
Adrian Setyadi <a.styd@yahoo.com>
2020-11-27 22:42:14 +0700

    Send event when element no longer seen

---
+++ src/index+off-92.6+.js
@@ -0,0 +1,61 @@
+(window => {
+  const {
+    document,
+  } = window;
+
+  const elementsSelector = document.currentScript.getAttribute('elements-selector') || '.impression';
+  const visibleFraction = document.currentScript.getAttribute('visible-fraction') || 0.5;
+  const visibleFractioff = document.currentScript.getAttribute('visible-fractioff') || 0.0;
+
+  const ableToObserveIntersection =
+    'IntersectionObserver' in window &&
+    'IntersectionObserverEntry' in window &&
+    'intersectionRatio' in IntersectionObserverEntry.prototype;
+
+  if (ableToObserveIntersection) {
+    const intersectionConfig = {
+      threshold: [visibleFractioff, visibleFraction],
+    };
+
+    const intersectionCallback = (entries, self) => {
+      entries.forEach(({target, isIntersecting, intersectionRatio}) => {
+        const isImpressioned = target.getAttribute('impression') == ''
+        if (isIntersecting) {
+          if (!isImpressioned && intersectionRatio >= visibleFraction) {
+            fireImpression(target);
+          }
+
+          if (isImpressioned && intersectionRatio <= visibleFractioff) {
+            fireImpressioff(target);
+            self.unobserve(target);
+          }
+        } else {
+          if (isImpressioned) {
+            fireImpressioff(target);
+            self.unobserve(target);
+          }
+        }
+      });
+    };
+
+    const fireImpression = (element) => {
+      const impression = new Event('impression');
+      element.setAttribute('impression','');
+      element.dispatchEvent(impression);
+    }
+
+    const fireImpressioff = (element) => {
+      const impressioff = new Event('impressioff');
+      element.dispatchEvent(impressioff);
+      element.removeAttribute('impression');
+    }
+
+    const impressionObserver = new IntersectionObserver(intersectionCallback, intersectionConfig);
+
+    const elements = document.querySelectorAll(elementsSelector);
+
+    elements.forEach(element => {
+      impressionObserver.observe(element);
+    });
+  }
+})(window)
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,14 @@ var _92p6Config = Object.assign({}, defaultConfig, {
   }
 })
 
+var _92p6WithOffConfig = Object.assign({}, defaultConfig, {
+  entry: './src/index+off-92.6+.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'impression+off-92.6+.js',
+  }
+})
+
 module.exports = [
-  _92p6Config
+  _92p6Config, _92p6WithOffConfig
 ];
